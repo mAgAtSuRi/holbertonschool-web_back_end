1898d2c7efd80d8682257b4ce41e5a2696db2349
mAgAtSuRi <tristan.latuilliere@gmail.com>
2025-11-24 11:21:30 +0100

    float

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -6,6 +6,6 @@
 from typing import Tuple, Union
 
 
-def to_kv(k: str, v: Union[int, float]) -> Tuple[str, Union[int, float]]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """return tuple"""
     return tuple(k, float(v**2))
