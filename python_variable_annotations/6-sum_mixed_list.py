a50425a798534515449bc4ca6db0719bfcf955e7
mAgAtSuRi <tristan.latuilliere@gmail.com>
2025-11-24 10:47:44 +0100

    uniono

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -3,9 +3,9 @@
 """
 
 
-from typing import List
+from typing import List, Union
 
 
-def sum_mixed_list(mxd_list: List[float, int]) -> float:
+def sum_mixed_list(mxd_list: List[Union[float, int]]) -> float:
     """return sum"""
     return sum(mxd_list)
