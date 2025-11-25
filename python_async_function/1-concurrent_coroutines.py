13a774dd3b61b0f01fd02fd21cab1621b474c63d
Tristan <tristan.latuilliere@gmail.com>
2025-11-25 14:50:47 +0100

    formatted

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -2,16 +2,18 @@
 """
 This module defines an async coroutine that returns a random delay
 """
+import asyncio
 from basic_async_syntax import wait_random
-import asyncio, random
 
 
-async def wait_n(n, max_delay):
-	"""spawn wait_random n times with the specified max_delay."""
-	tasks = [wait_random(max_delay) for i in range(n)]
-	time_list = []
-	# append as soon as coroutine is finished
-	for task in asyncio.as_completed(tasks):
-		time = await task
-		time_list.append(time)
-	return time
+async def wait_n(n: int, max_delay: float):
+    """Spawn wait_random n times with the specified max_delay."""
+    tasks = [wait_random(max_delay) for _ in range(n)]
+    time_list = []
+
+    # Append delays in the order tasks finish
+    for task in asyncio.as_completed(tasks):
+        delay = await task
+        time_list.append(delay)
+
+    return time_list
