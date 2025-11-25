5193a5d0d5e8a928668036cfd2d8d553fdb04b5d
Tristan <tristan.latuilliere@gmail.com>
2025-11-25 11:55:59 +0100

    0 ft

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -1,7 +1,7 @@
+#!/user/bin/env python3
 import asyncio, random
 
 async def wait_random(max_delay = 10):
 	wait_delay = random.uniform(0, max_delay)
-	asyncio.wait(wait_delay)
+	await asyncio.sleep(wait_delay)
 	return wait_delay
-
