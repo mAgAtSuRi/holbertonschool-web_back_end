6fe520db0f9a2e4c361dc322049715478c9489f6
Tristan <tristan.latuilliere@gmail.com>
2025-11-25 12:00:11 +0100

    documented

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -1,6 +1,11 @@
+#!/usr/bin/env python3
+"""
+This module defines an async coroutine that returns a random delay
+"""
 import asyncio, random
 
 async def wait_random(max_delay: int = 10) -> float:
+	"""asynchronous routine returning random num"""
 	wait_delay = random.uniform(0, max_delay)
 	await asyncio.sleep(wait_delay)
 	return wait_delay
