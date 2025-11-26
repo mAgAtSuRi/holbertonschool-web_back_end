ebd872575a6d325e4b3dd4fdac134727d66623ec
mAgAtSuRi <tristan.latuilliere@gmail.com>
2025-11-26 10:52:59 +0100

    changed import

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -3,22 +3,7 @@
 This module defines an async coroutine that returns a random delay
 """
 import asyncio
-import random
-
-
-async def wait_random(max_delay: int = 10) -> float:
-    """
-    An asynchronous coroutine that returns a random delay
-
-    Args:
-    max_delay (int): default value is 10
-
-    Return:
-    A random delay between 0 and max_delay included
-    """
-    delay = random.uniform(0, max_delay)
-    await asyncio.sleep(delay)
-    return delay
+wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: float):
