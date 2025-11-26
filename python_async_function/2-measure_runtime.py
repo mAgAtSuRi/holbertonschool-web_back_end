d44bf04868f8bad31268e64d69f68962f23575f0
mAgAtSuRi <tristan.latuilliere@gmail.com>
2025-11-26 11:28:56 +0100

    used time

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python3
 """Module measuring time"""
 import asyncio
+import time
 wait_n = __import__("1-concurrent_coroutines").wait_n
 
 
 def measure_time(n, max_delay):
     """calculate time per operation"""
-    time_list = asyncio.run(wait_n(n, max_delay))
-    total_time = time_list[-1]
-    return total_time / n
+    start = time.time()
+    asyncio.run(wait_n(n, max_delay))
+    end = time.time()
+
+    total = end - start
+    return total / n
