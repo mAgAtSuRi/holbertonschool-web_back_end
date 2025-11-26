6abec44c3bfc557399aa7c2d2630db423146b39a
mAgAtSuRi <tristan.latuilliere@gmail.com>
2025-11-26 11:17:03 +0100

    manually sort even if its already sorted

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -2,19 +2,32 @@
 """
 This module defines an async coroutine that returns a random delay
 """
-from typing import List
 import asyncio
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: float) -> List[float]:
-    """Spawn wait_random n times with the specified max_delay."""
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """Spawn wait_random n times with the specified max_delay.
+    Return a list of delays in ascending order without using sort().
+    """
     tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
-    time_list = []
+    ordered_delays = []
 
-    # Append delays in the order tasks finish
-    for task in asyncio.as_completed(tasks):
-        delay = await task
-        time_list.append(delay)
+    # Get each delay as soon as each task finishes
+    for completed in asyncio.as_completed(tasks):
+        delay = await completed
 
-    return time_list
+        # Insert delay in ascending order
+        if not ordered_delays:
+            ordered_delays.append(delay)
+        else:
+            inserted = False
+            for i in range(len(ordered_delays)):
+                if delay < ordered_delays[i]:
+                    ordered_delays.insert(i, delay)
+                    inserted = True
+                    break
+            if not inserted:
+                ordered_delays.append(delay)
+
+    return ordered_delays
