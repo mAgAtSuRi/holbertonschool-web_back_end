f0e36799c761c262c485e0412b2dbcc720cfc21f
Tristan <tristan.latuilliere@gmail.com>
2025-11-27 10:56:05 +0100

    list

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 """async gene and coroutine"""
+from typing import List
 async_generator = __import__("0-async_generator").async_generator
 
 
-async def async_comprehension():
+async def async_comprehension() -> List[float]:
     """async comprehension"""
     return [x async for x in async_generator()]
