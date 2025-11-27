49c1d2a027f855453f5822b41af2ea84db9314aa
Tristan <tristan.latuilliere@gmail.com>
2025-11-27 11:26:12 +0100

    description

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -6,6 +6,7 @@ async_comprehension = __import__("1-async_comprehension").async_comprehension
 
 
 async def measure_runtime() -> float:
+    """Coroutine measuring time"""
     tasks = []
     start = time.time()
     for i in range(4):
