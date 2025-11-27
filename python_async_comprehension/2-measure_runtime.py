d9972374500f1d4980a03403bbff73474595c775
Tristan <tristan.latuilliere@gmail.com>
2025-11-27 11:25:13 +0100

    2 ft

---
+++ python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""measure time"""
+import time
+import asyncio
+async_comprehension = __import__("1-async_comprehension").async_comprehension
+
+
+async def measure_runtime() -> float:
+    tasks = []
+    start = time.time()
+    for i in range(4):
+        tasks.append(async_comprehension())
+    await asyncio.gather(*tasks)
+    end = time.time()
+    total_time = end - start
+    return total_time
