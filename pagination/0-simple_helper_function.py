35ddc398730c73239d678d3cc8d62e853b05283c
mAgAtSuRi <tristan.latuilliere@gmail.com>
2025-12-01 09:32:44 +0100

    removed first check

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -5,6 +5,4 @@ from typing import Tuple
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
     """function returning indexes at pages"""
-    if page <= 0:
-        page = 1
     return ((page - 1) * page_size, page * page_size)
