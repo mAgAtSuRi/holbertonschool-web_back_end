107e76e9adbef637b88632baa6d71081086eeef9
mAgAtSuRi <tristan.latuilliere@gmail.com>
2025-12-01 12:55:19 +0100

    formatted

--- pagination/3-hypermedia_del_pagination.py
+++ pagination/3-hypermedia_del_pagination.py
@@ -40,6 +40,7 @@ class Server:
         return self.__indexed_dataset
 
     def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
+        """Return a deletion-resilient hypermedia page."""
         assert isinstance(index, int) and index >= 0
         assert isinstance(page_size, int) and page_size > 0
 
