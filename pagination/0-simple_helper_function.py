80a647899d4ab369d9fac5f10067b60812966cb5
mAgAtSuRi <tristan.latuilliere@gmail.com>
2025-12-01 09:21:14 +0100

    0 ft

---
+++ pagination/0-simple_helper_function.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+from typing import Tuple
+"""Calculating indexes"""
+
+
+def index_range(page: int, page_size: int) -> Tuple:
+    """function returning indexes at pages"""
+    if page <= 0:
+        page = 1
+    return (page - 1, (page - 1) * page_size)
