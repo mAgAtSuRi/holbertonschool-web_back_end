597f5b30d68d3fe1d8668ba6e80b1ce59d41290d
Tristan <tristan.latuilliere@gmail.com>
2025-12-02 15:41:48 +0100

    topicS

--- NoSQL/11-schools_by_topic.py
+++ NoSQL/11-schools_by_topic.py
@@ -4,5 +4,5 @@
 
 def schools_by_topics(mongo_collection, topic):
     """returns the list of school having a specific topic:"""
-    result = mongo_collection.find({"topic": topic})
+    result = mongo_collection.find({"topics": topic})
     return list(result)
