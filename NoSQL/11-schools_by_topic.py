3c14284b134b057b684c83ea8901085ab747e46a
Tristan <tristan.latuilliere@gmail.com>
2025-12-02 15:39:54 +0100

    11 ft

---
+++ NoSQL/11-schools_by_topic.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""""Write a Python function that returns the list of school having a specific topic:"""
+
+
+def schools_by_topics(mongo_collection, topic):
+    """returns the list of school having a specific topic:"""
+    result = mongo_collection.find({"topic": topic})
+    return result
