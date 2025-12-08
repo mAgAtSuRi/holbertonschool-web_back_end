f71dd30274da52276e83fc1d8bc33a55b1820c54
mAgAtSuRi <tristan.latuilliere@gmail.com>
2025-12-08 11:24:28 +0100

    let

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -3,8 +3,8 @@ export default function taskBlock(trueOrFalse) {
   let task2 = true;
 
   if (trueOrFalse) {
-    let task = true;    // block-scoped, does not overwrite outer 'task'
-    let task2 = false;  // block-scoped, does not overwrite outer 'task2'
+    let task = true;
+    let task2 = false;
   }
 
   return [task, task2];
