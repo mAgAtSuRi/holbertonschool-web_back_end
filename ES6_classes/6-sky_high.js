ca8ee0dcd6abbe539d841f19c72d79c8734fab0b
Tristan <tristan.latuilliere@gmail.com>
2025-12-09 15:34:43 +0100

    super

--- ES6_classes/6-sky_high.js
+++ ES6_classes/6-sky_high.js
@@ -2,13 +2,13 @@ import Building from './5-building'
 
 export default class SkyHighBuilding extends Building {
 	constructor(sqft, floors) {
-		if (typeof sqft != 'number') {
+		if (typeof sqft !== 'number') {
 			throw new TypeError("Sqft must be a number")
 		}
-		if (typeof floors != 'number') {
+		if (typeof floors !== 'number') {
 			throw new TypeError ('Floors must be a number')
 		}
-		this._sqft = sqft;
+		super(sqft)
 		this._floors = floors;
 	}
 
