441fce7512ca127bea8e7ce8346b5a3d0085df63
Tristan <tristan.latuilliere@gmail.com>
2025-12-09 14:46:45 +0100

    4 ft

---
+++ ES6_classes/4-pricing.js
@@ -0,0 +1,51 @@
+import Currency from './3-currency.js';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    if (typeof amount !== "number") {
+      throw new TypeError("Amount must be a number");
+    }
+    if (!(currency instanceof Currency)) {
+      throw new TypeError("currency must be an instance of Currency");
+    }
+
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  // GETTERS
+  get amount() {
+    return this._amount;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  // SETTERS
+  set amount(value) {
+    if (typeof value !== "number") {
+      throw new TypeError("Amount must be a number");
+    }
+    this._amount = value;
+  }
+
+  set currency(value) {
+    if (!(value instanceof Currency)) {
+      throw new TypeError("currency must be an instance of Currency");
+    }
+    this._currency = value;
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.displayFullCurrency()}`;
+  }
+
+  convertPrice(conversionRate) {
+    return this._amount * conversionRate;
+  }
+}
+
+const p = new Pricing(100, new Currency("EUR", "Euro"));
+console.log(p);
+console.log(p.displayFullPrice());
