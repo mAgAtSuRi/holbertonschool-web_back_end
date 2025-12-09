671d0551baff8424c9ec15d6c9bb444e4365f4ab
Tristan <tristan.latuilliere@gmail.com>
2025-12-09 14:28:29 +0100

    3 ft

---
+++ ES6_classes/3-currency.js
@@ -0,0 +1,26 @@
+export default class Currency {
+	constructor(code, name) {
+		this._name = name;
+		this._code = code;
+	}
+
+	get name() {
+		return this._name
+	}
+	get code() {
+		return this._code
+	}
+
+	set name(value) {
+		this._name = value
+	}
+	set code(value) {
+		this._code = value
+	}
+
+	displayFullCurrency() {
+		return `${this._name} (${this._code})`
+	}
+}
+const dollar = new Currency('$', 'Dollars')
+console.log(dollar.displayFullCurrency())
