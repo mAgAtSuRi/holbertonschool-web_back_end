003765ac9b6a3b64f4fd544934671108e005b0da
mAgAtSuRi <tristan.latuilliere@gmail.com>
2025-12-19 12:14:23 +0100

    6 ft

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,9 +1,9 @@
 import signUpUser from "./4-user-promise.js"
 import uploadPhoto from "./5-photo-reject.js"
 export default function handleProfileSignup(firstName, lastName, filename) {
-	const result1 = signUpUser(firstName, lastName)
-	return result1
-	.then(() => uploadPhoto(filename))
+	return Promise.allSettled([
+		signUpUser(firstName, lastName),
+		uploadPhoto(filename)
+	])
 }
-
 // console.log(handleProfileSignup("Bob", "Dylan", "bob_dylan.jpg"));
