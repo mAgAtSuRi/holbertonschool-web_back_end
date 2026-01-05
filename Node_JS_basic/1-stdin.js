d58236f2b69aa58aa8a4f728576d16cf0d198f38
mAgAtSuRi <tristan.latuilliere@gmail.com>
2026-01-05 10:54:48 +0100

    resume

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,6 +1,7 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.setEncoding('utf8');
+process.stdin.resume();
 
 process.stdin.on('data', (data) => {
   process.stdout.write(`Your name is: ${data.trim()}\n`);
@@ -9,5 +10,3 @@ process.stdin.on('data', (data) => {
 process.stdin.on('end', () => {
   process.stdout.write('This important software is now closing\n');
 });
-
-module.exports = {};
