f4b948253fcea3d932b7d86767ae4011b0c90882
Tristan <tristan.latuilliere@gmail.com>
2026-01-06 14:45:37 +0100

    test

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,14 +1,7 @@
-process.stdout.write('Welcome to Holberton School, what is your name?\n');
-
-process.stdin.setEncoding('utf8');
-process.stdin.resume();
-
+console.log('Welcome to Holberton School, what is your name?');
 process.stdin.on('data', (data) => {
-  process.stdout.write(`Your name is: ${data.trim()}\n`);
+  process.stdout.write(`Your name is: ${data}`);
 });
-
-process.stdin.on('end', () => {
-  process.stdout.write('This important software is now closing\n');
+process.on('exit', () => {
+  console.log('This important software is now closing');
 });
-
-module.exports = {};
